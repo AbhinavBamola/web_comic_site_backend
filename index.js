bbdfacb87586d893c18c47a9e49700506fc4768b
AbhinavBamola <abhinavbamola2405@gmail.com>
2025-08-02 18:30:19 +0530

    Initial commmit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const port=process.env.PORT||8000;
 const mongourl=process.env.MONGO_URL||"mongodb://127.0.0.1:27017/manga_db";
 const app=express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/manga_db')
+mongoose.connect(mongourl)
         .then(() => console.log('Connected to MongoDB...'))
         .catch(err => console.error('Could not connect to MongoDB...'));
 
